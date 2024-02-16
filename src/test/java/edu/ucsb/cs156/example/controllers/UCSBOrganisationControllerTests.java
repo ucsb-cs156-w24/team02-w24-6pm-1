afe12724b76c69ba1a8cb3e2b8d3eaac979f3f32
sanilkatula <124859948+sanilkatula@users.noreply.github.com>
2024-02-15 18:51:55 -0800

    sk - mutation 100%

--- src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganisationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganisationControllerTests.java
@@ -262,9 +262,9 @@ public class UCSBOrganisationControllerTests extends ControllerTestCase {
                                 .build();
 
                 UCSBOrganisation OISSEdited = UCSBOrganisation.builder()
-                                .orgCode("OISS Edit")
-                                .orgTranslationShort("INTERNATIONAL STUDENTS")
-                                .orgTranslation("OFFICE OF INTERNATIONAL STUDENTS AND SCHOLARS")
+                                .orgCode("OISS")
+                                .orgTranslationShort("INTERNATIONAL STUDENTSs")
+                                .orgTranslation("OFFICE OF INTERNATIONAL STUDENTS AND SCHOLARSs")
                                 .inactive(true)
                                 .build();
 
